e355775f2fd1e50ab52148f704b54f0d50510cc2
Katleho <katlehomokhethi1@gmail.com>
2025-02-11 19:27:31 +0200

    Add Business Logic

--- users/src/api/users.py
+++ users/src/api/users.py
@@ -1,42 +1,92 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 import json
+import uuid
+import os
+import boto3
+from datetime import datetime
 
-# import requests
-
+# Prepare DynamoDB client
+USERS_TABLE = os.getenv('USERS_TABLE', None)
+dynamodb = boto3.resource('dynamodb')
+ddbTable = dynamodb.Table(USERS_TABLE)
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    route_key = f"{event['httpMethod']} {event['resource']}"
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    # Set default response, override with data from DynamoDB if any
+    response_body = {'Message': 'Unsupported route'}
+    status_code = 400
+    headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+        }
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    try:
+        # Get a list of all Users
+        if route_key == 'GET /users':
+            ddb_response = ddbTable.scan(Select='ALL_ATTRIBUTES')
+            # return list of items instead of full DynamoDB response
+            response_body = ddb_response['Items']
+            status_code = 200
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+        # CRUD operations for a single User
+       
+        # Read a user by ID
+        if route_key == 'GET /users/{userid}':
+            # get data from the database
+            ddb_response = ddbTable.get_item(
+                Key={'userid': event['pathParameters']['userid']}
+            )
+            # return single item instead of full DynamoDB response
+            if 'Item' in ddb_response:
+                response_body = ddb_response['Item']
+            else:
+                response_body = {}
+            status_code = 200
+        
+        # Delete a user by ID
+        if route_key == 'DELETE /users/{userid}':
+            # delete item in the database
+            ddbTable.delete_item(
+                Key={'userid': event['pathParameters']['userid']}
+            )
+            response_body = {}
+            status_code = 200
+        
+        # Create a new user 
+        if route_key == 'POST /users':
+            request_json = json.loads(event['body'])
+            request_json['timestamp'] = datetime.now().isoformat()
+            # generate unique id if it isn't present in the request
+            if 'userid' not in request_json:
+                request_json['userid'] = str(uuid.uuid1())
+            # update the database
+            ddbTable.put_item(
+                Item=request_json
+            )
+            response_body = request_json
+            status_code = 200
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+        # Update a specific user by ID
+        if route_key == 'PUT /users/{userid}':
+            # update item in the database
+            request_json = json.loads(event['body'])
+            request_json['timestamp'] = datetime.now().isoformat()
+            request_json['userid'] = event['pathParameters']['userid']
+            # update the database
+            ddbTable.put_item(
+                Item=request_json
+            )
+            response_body = request_json
+            status_code = 200
+    except Exception as err:
+        status_code = 400
+        response_body = {'Error:': str(err)}
+        print(str(err))
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'statusCode': status_code,
+        'body': json.dumps(response_body),
+        'headers': headers
     }
